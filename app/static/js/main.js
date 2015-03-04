13658b1f5cdd85623e774d56a8fe04ca549e253f
john@drkurt.com <john@drkurt.com>
2015-03-04 14:38:26 -0800

    working on a twitter widget

---
+++ app/static/js/main.js
@@ -0,0 +1,12 @@
+/*javascript*/
+
+var main = function(){
+window.alert("hello");
+
+var height=document.getElementById('info').offsetHeight;
+var timeline = document.getElementsByClassName('twitter-timeline');
+
+
+}
+
+$(document).ready(main)
